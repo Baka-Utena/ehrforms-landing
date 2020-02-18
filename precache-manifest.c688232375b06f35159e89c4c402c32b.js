5da7f8be4b05273b0f7e7452495a650a6012cb0e
Svetlana Khudina <skhudina@solit-clouds.ru>
2020-02-18 22:51:37 +0300

    Updates

---
+++ precache-manifest.c688232375b06f35159e89c4c402c32b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57df93af58563b22c07f836f331ee2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63a2c34c0988dfae2d32",
+    "url": "/static/css/main.976222f8.chunk.css"
+  },
+  {
+    "revision": "59e94bc1d7f95aaf3c4e",
+    "url": "/static/js/2.8a8aad28.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.8a8aad28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63a2c34c0988dfae2d32",
+    "url": "/static/js/main.fd4d4d5d.chunk.js"
+  },
+  {
+    "revision": "4ead53829ac09c3c53b5",
+    "url": "/static/js/runtime-main.0423af17.js"
+  },
+  {
+    "revision": "d89719ce828895c40ed6bb76524925cb",
+    "url": "/static/media/arrows.d89719ce.png"
+  },
+  {
+    "revision": "aa6ad31c9a469649e0e370e643b97213",
+    "url": "/static/media/example.aa6ad31c.png"
+  },
+  {
+    "revision": "6972b8d188ce62897570104810d86d80",
+    "url": "/static/media/image_bottom.6972b8d1.png"
+  },
+  {
+    "revision": "086a8f2af622bc527b3c060933f37567",
+    "url": "/static/media/image_top.086a8f2a.png"
+  },
+  {
+    "revision": "11ebcfa25537d5812b9f32d9e224a469",
+    "url": "/static/media/infogr.11ebcfa2.png"
+  },
+  {
+    "revision": "b92005098b40f225fd0cf4eec0653edf",
+    "url": "/static/media/shadow.b9200509.png"
+  }
+]);
