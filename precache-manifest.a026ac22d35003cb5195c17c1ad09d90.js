3177e2f3e49ca8efd18243c7efb8dae8d65dfb6a
Svetlana Khudina <skhudina@solit-clouds.ru>
2020-03-18 20:17:01 +0300

    Updates

---
+++ precache-manifest.a026ac22d35003cb5195c17c1ad09d90.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d5c920a83f0ec632afd550e4b5b6ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "893d5510202b11b0c9f0",
+    "url": "/static/css/main.d7e8a30c.chunk.css"
+  },
+  {
+    "revision": "c03d5c4e88c26a0aed17",
+    "url": "/static/js/2.3bc70f1a.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.3bc70f1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "893d5510202b11b0c9f0",
+    "url": "/static/js/main.516ce257.chunk.js"
+  },
+  {
+    "revision": "4ead53829ac09c3c53b5",
+    "url": "/static/js/runtime-main.0423af17.js"
+  },
+  {
+    "revision": "d89719ce828895c40ed6bb76524925cb",
+    "url": "/static/media/arrows.d89719ce.png"
+  },
+  {
+    "revision": "aa6ad31c9a469649e0e370e643b97213",
+    "url": "/static/media/example.aa6ad31c.png"
+  },
+  {
+    "revision": "6972b8d188ce62897570104810d86d80",
+    "url": "/static/media/image_bottom.6972b8d1.png"
+  },
+  {
+    "revision": "086a8f2af622bc527b3c060933f37567",
+    "url": "/static/media/image_top.086a8f2a.png"
+  },
+  {
+    "revision": "11ebcfa25537d5812b9f32d9e224a469",
+    "url": "/static/media/infogr.11ebcfa2.png"
+  },
+  {
+    "revision": "b92005098b40f225fd0cf4eec0653edf",
+    "url": "/static/media/shadow.b9200509.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ehrforms-landing/precache-manifest.d434b3799b1ff34a9c145cb23ae36f29.js"
+  "/precache-manifest.a026ac22d35003cb5195c17c1ad09d90.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ehrforms-landing/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
